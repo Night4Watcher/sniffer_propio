ceab8f850ce7694be65eb2ce39181a827b32e734
Night4Watcher <andoni.hilario.lazaro@gmail.com>
2025-04-24 17:36:18 +0200

    Sniffer ajustado para la captura de informacion general del paquete

--- main.py
+++ main.py
@@ -1,35 +1,83 @@
+"""
+Sniffer de red personalizado hecho tomando como base del proyecto el sniffer en el
+siguiente repositorio de github -> https://github.com/Chispasgg/euneiz-redes1
+
+Requisitos:
+- Tener pyshark instalado
+- 
+"""
+
 import pyshark
 
-def gestion_paquetes(captura_paquetes):
-    variables_paquete = []
-    contador_paquetes = 0
-    for paquetes in captura_paquetes:
-        contador_paquetes += 1
-        print(f"\n Paquete {contador_paquetes}")
+def almacenamiento_paquetes(paquetes):
+    """
+    Mediante esta funcion se busca lograr dividir la informacion del paquete capturado
+    en diferentes secciones para luego poder procesar dichos paquetes.
+    
+    Keyword arguments:
+    paquetes -- captura de los paquetes en transito en la interfaz de red seleccionada
+    Return: return_description
+    """
+    
+    # esto es el diccionario anidado donde almacenar toda la informacion del paquete capturado
+    informacion_paquetes = {
         
-        for i, capa in enumerate(paquetes.layers, start=1):
-            print(f"🔹 Capa {i}: {capa.layer_name}")
+    }
+    # variables de contabilizacion
+    paquetes_analizados = 0
+    campo_informacion_general = 0
+    # bucle con el que recorrer todos los paquetes que vayan siendo capturados
+    for paquete in paquetes:
+        paquetes_analizados += 1
+        print(f'Paquete analizado num {paquetes_analizados}')
+        
+        # Obtencion de la informacion general del paquete
+        info_general = paquete.frame_info
+        for campo_informacion in info_general.field_names:
+            campo_informacion_general += 1
+            print(f'Campo de informacion num {campo_informacion_general}.')
+            print(getattr(info_general, campo_informacion))
+            # se introduce el valor obtenido en el diccionario anidado 'info_general'
+            informacion_paquetes['info_general'] = getattr(info_general, campo_informacion)
             
-            for campo in capa.field_names:
-                valor = getattr(capa, campo)
-                print(f"          - {campo}: {valor}")
+        
+        # obtencion de todas las capas que se encuentran disponibles en el paquete capturado
+        capas_disponibles_paquete = paquete.layers
+        # bucle con el que recorrer todas las capas disponibles en el paquete
+        
 
-
-def obtencion_paquetes(tarjeta_red):
+def captura_paquetes():
+    """
+    Mediante esta funcion se realiza la captura de los distintos paquetes que transitan
+    por la interfaz de red seleccionada. Para luego ser enviados para su almacenamiento
+    y gestion.
+    
+    Keyword arguments:
+    interfaz -- interfaz de red sobre la que trabajara el sniffer
+    """
+    
+    interfaz = input("¿Cual es tu tarjeta de red?")
     try:
-        captura_paquetes = pyshark.LiveCapture(
-            interface=tarjeta_red
+        paquetes_capturados = pyshark.LiveCapture(
+            interface=interfaz
         )
     except PermissionError:
-        print("Revisa los permisos o usa sudo")
+        print("Revisa los permisos concedidos.")
         exit(0)
-    print("Vamos a gestionar los paquetes.")
-    gestion_paquetes(captura_paquetes)
+    print("Vamos a almacenar la informacion de los paquetes.")
+    almacenamiento_paquetes(paquetes_capturados)
 
-def main():
-    interfaz_red = input("¿Cual es tu interfaz de red?")
-    obtencion_paquetes(interfaz_red)
-
-if __name__ == "__main__":
-    print("Ejecutando el sniffer...")
-    main()
+# Este va a ser el bloque o la funcion 'main' del programa
+if __name__ == '__main__':
+    print("")
+    print("  _           _____   _____  _____  ")
+    print(" | |         |  __ \ / ____|/ ____| ")
+    print(" | |__  _   _| |__) | |  __| |  __  ")
+    print(" | '_ \| | | |  ___/| | |_ | | |_ | ")
+    print(" | |_) | |_| | |    | |__| | |__| | ")
+    print(" |_.__/ \__, |_|     \_____|\_____| ")
+    print("         __/ |                      ")
+    print("        |___/                       ")
+    print("")
+    print("Sniffer arrancado.")
+    captura_paquetes()          
